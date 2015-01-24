c62c1fae3c5fec08379e913f4c214a24f9377208
tamirz12345 <tamirz12345@gmail.com>
2015-01-24 17:05:00 +0200

    שינוי תמונה דסקית

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
@@ -18,8 +19,7 @@ public class MyGdxGame extends ApplicationAdapter {
 	Rectangle p1Rec;
 	private Music music;
 	
-	
-	
+
 	
 	@Override
 	public void create () {
