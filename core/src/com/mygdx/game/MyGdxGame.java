ab754b1864fd0635e01b55159076e2113b48afa0
tamirz12345 <tamirz12345@gmail.com>
2015-01-24 16:00:38 +0200

    עדכון touch
    עדכנתי touch שייה פה

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -3,17 +3,35 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	Texture player1;
+	private Vector3 p1Pos;
+	private OrthographicCamera camera;
+	Rectangle p1Rec;
+	
+	
+	
+	
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("player.jpg");
+		player1 = new Texture("player.jpg");
+		p1Pos= new Vector3(0, 240, 0);
+		camera = new OrthographicCamera();
+	    camera.setToOrtho(false, 800, 480);
+	    
+	    
+	    
+	    
 	}
 
 	@Override
@@ -21,7 +39,16 @@ public class MyGdxGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(player1, p1Pos.x, p1Pos.y);
 		batch.end();
+		
+		if(Gdx.input.isTouched()) {
+	          p1Pos.set(Gdx.input.getX(), Gdx.input.getY(),0);
+	          camera.unproject(p1Pos);
+	          
+	    }
+		
+		
+		
 	}
 }
