5e8167f0158627acdded66ba14f43e1e0df8cf20
tamirz12345 <tamirz12345@gmail.com>
2015-02-02 18:49:19 +0200

    Animated move

--- core/src/GameObjects/Tool.java
+++ core/src/GameObjects/Tool.java
@@ -9,6 +9,7 @@ import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 
 /**
@@ -18,30 +19,56 @@ import com.badlogic.gdx.math.Vector3;
 public class Tool {
     public Texture img;
     public Rectangle rec;
-    
+    Vector2 target;
+    Vector2 delta;
+    final int TimeToMove = 5;
+    int moves;
     public Tool(String image_path) {
         this.img = new Texture(Gdx.files.internal(image_path));
-        this.rec = new Rectangle(0, 240, 64, 64);
+        this.rec = new Rectangle(700, 240, 64, 64);
+        target = null;
+        delta = null;
+        moves = 0;
+        
     }
 
     public void move(float x, float y) {
     	float temp = (float) (Gdx.graphics.getWidth()*0.5);
-    	/*
-    	if (x <= Gdx.graphics.getWidth() * 0.5)
+    	target = new Vector2();
+    	target.x = (float) Math.max(x, Gdx.graphics.getWidth() * 0.5)   - rec.height / 2;
+    	target.y = y - rec.width / 2 ;
+    	delta = new Vector2();
+    	delta.x = (target.x - rec.x) / TimeToMove;
+    	delta.y =  (target.y - rec.y) / TimeToMove;
+    	moves = 0;
+        
+    }
+    
+    public void update()
+    {
+    	if (target == null)
     	{
-    		this.rec.x = x + rec.height / 2 ;
-    		
+    		return;
+    	}
+    	if (moves < TimeToMove)
+    	{
+    		rec.x += delta.x;
+        	rec.y += delta.y;
+        	moves++;
+        	
     	}
     	else
     	{
-    		this.rec.x = temp - this.rec.height;
-    		
+    		moves = 0;
+    		target = null;
+    		delta = null;
     	}
-    	*/ 
-    	this.rec.x = (float) Math.min(x, Gdx.graphics.getWidth() * 0.5)   - rec.height / 2;
-    	this.rec.y = y - rec.width / 2 ;
     	
-        
+    	
+    	
+    	
+    	
+    	
     }
  
 
