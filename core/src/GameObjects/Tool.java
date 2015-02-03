50a41717504fa58ad935ed4262821b6b8e64e1be
yuvalzarad <yuvalzarad@gmail.com>
2015-02-03 11:28:55 +0200

    'Limit' class added
    - The validation of the tool coords is being calaulated using the
    PERCENT of the limit line on the screen.
    - The tool widht and height are now being getted as parameters, no usage
    of numbers such as 64.
    - bug: the bot tool doesnt shot

--- core/src/GameObjects/Tool.java
+++ core/src/GameObjects/Tool.java
@@ -1,16 +1,9 @@
 package GameObjects;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.math.Vector3;
 
 /**
  * Created by user-pc on 25/01/2015.
@@ -21,27 +14,35 @@ public class Tool {
     public Rectangle rec;
     Vector2 target;
     Vector2 delta;
+    boolean isBelow;
     final int TimeToMove = 5;
     int moves;
-    public Tool(String image_path) {
+    Limits limit = new Limits();
+
+    public Tool(String image_path, boolean isBelow, int height, int width) {
         this.img = new Texture(Gdx.files.internal(image_path));
-        this.rec = new Rectangle(700, 240, 64, 64);
+        this.isBelow = isBelow;
+
+        if (this.isBelow) {
+            this.rec = new Rectangle(700, 240, width, height);
+        }
+        else {
+          this.rec = new Rectangle(100, 240, width, height);
+        }
+
         target = null;
         delta = null;
         moves = 0;
-        
     }
 
     public void move(float x, float y) {
-    	float temp = (float) (Gdx.graphics.getWidth()*0.5);
-    	target = new Vector2();
-    	target.x = (float) Math.max(x, Gdx.graphics.getWidth() * 0.5)   - rec.height / 2;
-    	target.y = y - rec.width / 2 ;
+    	target = new Vector2(x,y);
+    	target = limit.validateTool(target, this.isBelow, this.rec.getHeight(), this.rec.getWidth(), 50);
+
     	delta = new Vector2();
     	delta.x = (target.x - rec.x) / TimeToMove;
     	delta.y =  (target.y - rec.y) / TimeToMove;
     	moves = 0;
-        
     }
     
     public void update()
@@ -55,7 +56,6 @@ public class Tool {
     		rec.x += delta.x;
         	rec.y += delta.y;
         	moves++;
-        	
     	}
     	else
     	{
@@ -63,14 +63,31 @@ public class Tool {
     		target = null;
     		delta = null;
     	}
-    	
-    	
-    	
-    	
-    	
-    	
     }
- 
 
 
+/**
+ * Created by user-pc on 02/02/2015.
+ */
+public static class Limits {
+
+    public Limits()
+    {}
+
+    public Vector2 validateTool(Vector2 pos, boolean isBelow, float height, float width, int limitPercent)
+    {
+        Vector2 valid = new Vector2();
+        if (isBelow) {
+            valid.x = (float) Math.max(pos.x, Gdx.graphics.getWidth() * (limitPercent / 100))   - width / 2;
+            valid.y = pos.y - height / 2 ;
+        }
+
+        else {
+            valid.x = (float) Math.min(pos.x, Gdx.graphics.getWidth() * (limitPercent / 100))   - width / 2;
+            valid.y = pos.y - height / 2 ;
+        }
+
+        return valid;
+    }
+    }
 }
--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -23,19 +23,25 @@ public class MyGdxGame extends ApplicationAdapter {
 	private OrthographicCamera camera;
 	private Music music;
 	Tool t1;
+    Tool bot;
 	Vector3 tempTouch;
 	float height,width;
 	ShapeRenderer shaper;
-	@Override
+    final int TOOL_WIDTH = 64;
+    final int TOOL_HEIGHT = 64;
+
+    @Override
 	public void create () {
 		batch = new SpriteBatch();
 		camera = new OrthographicCamera();
-	    //camera.setToOrtho(false, 800, 480);
-		height= Gdx.graphics.getWidth();
-		width= Gdx.graphics.getHeight();
-		camera.setToOrtho(false, height,width);
-        t1 = new Tool("player1.png");
-        tempTouch= new Vector3();
+		height = Gdx.graphics.getWidth();
+		width = Gdx.graphics.getHeight();
+		camera.setToOrtho(false, height, width);
+
+        t1 = new Tool("player1.png", true, TOOL_HEIGHT, TOOL_WIDTH);
+        bot = new Tool("player1.png", false, TOOL_HEIGHT, TOOL_WIDTH);
+        tempTouch = new Vector3();
+
 	    music = Gdx.audio.newMusic(Gdx.files.internal("backroundMusic.mp3"));
 	    music.setLooping(true);
 	    music.play();
@@ -46,22 +52,20 @@ public class MyGdxGame extends ApplicationAdapter {
 	public void render () {
 		Gdx.gl.glClearColor(1, 10, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
 		t1.update();
-		
+
 		shaper.begin(ShapeType.Line);
 		shaper.line(height/2, 0, height/2, width,Color.BLACK,Color.BLACK);
-		
 		shaper.end();
 		
 		batch.begin();
 		batch.draw(t1.img, t1.rec.x, t1.rec.y);
 		batch.end();
-		DragListener dl = new DragListener();
+
 		if(Gdx.input.isTouched()) {
-			
 			camera.unproject(tempTouch.set(Gdx.input.getX(), Gdx.input.getY(),0));
             t1.move(tempTouch.x, tempTouch.y);
-	        
 	    }
 	}
 }
