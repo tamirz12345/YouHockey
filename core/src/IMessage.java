8566dbbb8a1ebe290dde1de823201816a051e7ef
tamirz12345 <tamirz12345@gmail.com>
2015-03-01 19:20:44 +0200

    Interface

--- core/src/IMessage.java
+++ core/src/IMessage.java
@@ -1,6 +1,7 @@
+import java.util.List;
 
 public interface IMessage {
 	public String toString();
-	public String getType();
-	
+	public String getType(); // returns the number of the command
+	public List<String> getParameters(); 
 }
