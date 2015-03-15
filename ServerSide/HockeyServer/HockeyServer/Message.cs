0a85a13121783e4c7d19e9f9895e0ac7730fd492
yuvalzarad <yuvalzarad@gmail.com>
2015-03-15 19:28:34 +0200

    Message class added

---
+++ ServerSide/HockeyServer/HockeyServer/Message.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HockeyServer
+{
+    class Message
+    {
+        string message;
+
+        public Message(byte[] arr)
+        {
+            this.message = Encoding.UTF8.GetString(arr, 0, arr.Length);
+        }
+
+        public Message(string message)
+        {
+           this.message = message;
+        }
+
+        public string cutOpcode() // returns the command number
+        {
+            return this.message.Substring(0, message.IndexOf('-'));
+        }
+    }
+}
