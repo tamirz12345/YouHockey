d78a4cd875bc846ab7dfe022b20d373710deebc8
yuvalzarad <yuvalzarad@gmail.com>
2015-03-15 19:08:01 +0200

    אסתטיקה

--- ServerSide/HockeyServer/HockeyServer/Server.cs
+++ ServerSide/HockeyServer/HockeyServer/Server.cs
@@ -14,70 +14,19 @@ namespace HockeyServer
 {
     class Server
     {
-        public TcpListener tcpListener;
         private Thread listenThread;
-        private NetworkStream clientStream;
 
         public Server(string ip, int port)
         {
 
         }
-
-        private void ListenForClients()
-        {
-            //this.tcpListener.Start();
-
-            while (true)
-            {
-                //blocks until a client has connected to the server
-               // this.unmatchedClients.Enqueue(this.tcpListener.AcceptTcpClient());
-
-                //create a thread to handle communication
-                //with connected client
-                //Thread clientThread = new Thread(new ParameterizedThreadStart(HandleClientComm));
-                //clientThread.Start(client);
-            }
-        }
-
-
-        private void HandleClientComm(object client)
-        {
-            TcpClient tcpClient = (TcpClient)client;
-            this.clientStream = tcpClient.GetStream();
-
-            //this.sendScreenshot = new Thread(new ThreadStart(sendData));
-            //this.sendScreenshot.Start();
-
-            byte[] message = new byte[4096];
-            int bytesRead;
-
-            while (true)
-            {
-                bytesRead = 0;
-
-                try
-                {
-                    //blocks until a client sends a message
-
-                    bytesRead = clientStream.Read(message, 0, 4096);
-                }
-                catch (Exception)
-                {
-                    //a socket error has occured
-                    break;
-                }
-
-                if (bytesRead == 0)
-                {
-                    //the client has disconnected from the server
-                    break;
-                }
-
-                //message has successfully been received
-                ASCIIEncoding encoder = new ASCIIEncoding();
-                //frm2.appendLine("Server recieved: " + encoder.GetString(message, 0, bytesRead));
-            }
-        }
     }
+
+    /*
+     * how to use a thread:
+     * this.sendScreenshot = new Thread(new ThreadStart(sendData));
+     * this.sendScreenshot.Start();
+     */
+      
 }
 
