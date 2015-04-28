bde698fed82ada7894c7343ea4c9d7487bbb02c6
yuvalzarad <yuvalzarad@gmail.com>
2015-04-28 18:02:04 +0300

    null message handled

--- ServerSide/HockeyServer/HockeyServer/Server.cs
+++ ServerSide/HockeyServer/HockeyServer/Server.cs
@@ -51,54 +51,58 @@ namespace HockeyServer
 
         void handleCommand(Socket client)
         {
-            string toSend; 
+            string toSend;
             while (true)
             {
-                IPEndPoint clientep = (IPEndPoint)client.RemoteEndPoint; 
+                IPEndPoint clientep = (IPEndPoint)client.RemoteEndPoint;
                 byte[] data = new byte[1024];
                 int recv = client.Receive(data);
                 Message msg = new Message(data);
-                Mutex m = new Mutex();
-                string opcode = msg.cutOpcode();
-                m.WaitOne();
-                Console.WriteLine("Message: '" + msg.message + "' Received from: " + clientep.ToString());
-                m.ReleaseMutex();
-                List<string> parameters = msg.cutParameters();
-                
 
-                if (opcode == "660")
+                if (msg.message != null)
                 {
-                    this.pairQueue.insertClient(new ClientInfo(client, clientep.ToString()));
-                    toSend = "300-\n";
-                    data = Encoding.ASCII.GetBytes(toSend);
-                    client.Send(data, toSend.Length, SocketFlags.None);
-                }
+                    Mutex m = new Mutex();
+                    string opcode = msg.cutOpcode();
+                    m.WaitOne();
+                    Console.WriteLine("Message: '" + msg.message + "' Received from: " + clientep.ToString());
+                    m.ReleaseMutex();
+                    List<string> parameters = msg.cutParameters();
 
-                if (opcode == "405")
-                {
-                    this.pairQueue.deleteClient((new ClientInfo(null, clientep.ToString())));
-                }
 
-                if (opcode == "661")
-                {
-                    string port = parameters[1];
-                    Pair p = this.pairQueue.getSpecifiedPair(new ClientInfo(client, clientep.ToString()));
-                    this.pairQueue.addPortToPair(Convert.ToInt32(port), new ClientInfo(client, clientep.ToString()));
+                    if (opcode == "660")
+                    {
+                        this.pairQueue.insertClient(new ClientInfo(client, clientep.ToString()));
+                        toSend = "300-\n";
+                        data = Encoding.ASCII.GetBytes(toSend);
+                        client.Send(data, toSend.Length, SocketFlags.None);
+                    }
 
-                    // sending to the listener:
-                    string ipInitiator = p.initiator.ip;
-                    string[] words = ipInitiator.Split(':');
-                    toSend = "301-" + words[0] + "- \n";
-                    data = Encoding.ASCII.GetBytes(toSend);
-                    p.listener.socket.Send(data, toSend.Length, SocketFlags.None);
-                    
-                    // sending to the initiator:
-                    string ipListener = p.listener.ip;
-                    ipListener.Split(':');
-                    toSend = "301-" + words[0] + "-" + port + "-\n";
-                    data = Encoding.ASCII.GetBytes(toSend);
-                    p.initiator.socket.Send(data, toSend.Length, SocketFlags.None);
-                    
+                    if (opcode == "405")
+                    {
+                        this.pairQueue.deleteClient((new ClientInfo(null, clientep.ToString())));
+                    }
+
+                    if (opcode == "661")
+                    {
+                        string port = parameters[1];
+                        Pair p = this.pairQueue.getSpecifiedPair(new ClientInfo(client, clientep.ToString()));
+                        this.pairQueue.addPortToPair(Convert.ToInt32(port), new ClientInfo(client, clientep.ToString()));
+
+                        // sending to the listener:
+                        string ipInitiator = p.initiator.ip;
+                        string[] words = ipInitiator.Split(':');
+                        toSend = "301-" + words[0] + "- \n";
+                        data = Encoding.ASCII.GetBytes(toSend);
+                        p.listener.socket.Send(data, toSend.Length, SocketFlags.None);
+
+                        // sending to the initiator:
+                        string ipListener = p.listener.ip;
+                        ipListener.Split(':');
+                        toSend = "301-" + words[0] + "-" + port + "-\n";
+                        data = Encoding.ASCII.GetBytes(toSend);
+                        p.initiator.socket.Send(data, toSend.Length, SocketFlags.None);
+
+                    }
                 }
             }
         }
