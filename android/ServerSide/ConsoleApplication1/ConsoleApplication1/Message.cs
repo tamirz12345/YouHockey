0361737318efb881d6e3069f0325837e6366cc5f
yuvalzarad <yuvalzarad@gmail.com>
2015-05-10 18:31:53 +0300

    console server

---
+++ android/ServerSide/ConsoleApplication1/ConsoleApplication1/Message.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Message
+    {
+        public string message;
+
+        public Message(byte[] arr)
+        {
+            try
+            {
+                this.message = Encoding.UTF8.GetString(arr, 0, arr.Length);
+                this.message.Substring(0, message.LastIndexOf('-') + 1);
+            }
+            catch (Exception e)
+            {
+                this.message = null;
+            }
+        }
+
+        public Message(string message)
+        {
+            this.message = message;
+        }
+
+        public string cutOpcode() // returns the command number
+        {
+            try
+            {
+                return this.message.Substring(0, message.IndexOf('-'));
+            }
+            catch (Exception e)
+            {
+                return "";
+            }
+        }
+
+        public List<string> cutParameters()
+        {
+            string[] words = this.message.Split('-');
+            List<string> parameters = new List<string>();
+
+            foreach (string word in words)
+            {
+                parameters.Add(word);
+            }
+
+            return parameters;
+        }
+
+    }
+}
---
+++ android/ServerSide/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int port = 3000;
+            Server server = new Server(port);
+
+        }
+    }
+}
