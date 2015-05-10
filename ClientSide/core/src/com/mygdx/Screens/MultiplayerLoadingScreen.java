047b8df5b0cfc33c3399ff9c66668ff771f615d1
yuvalzarad <yuvalzarad@gmail.com>
2015-05-10 18:51:58 +0300

    new server file

--- ClientSide/core/src/com/mygdx/Screens/MultiplayerLoadingScreen.java
+++ ClientSide/core/src/com/mygdx/Screens/MultiplayerLoadingScreen.java
@@ -83,7 +83,7 @@ public class MultiplayerLoadingScreen  extends ScreenAdapter{
 	
 	public class ServerChat extends AsyncTask<String, Void, String> {
 	  	public int portS = 3000;
-	  	
+	  	public String ipS = "192.168.0.103"; 
 	  	InetSocketAddress serverAddress;
 	  	String sentence;
 	  	String recivedString;
