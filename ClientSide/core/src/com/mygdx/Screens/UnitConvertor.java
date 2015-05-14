f1c33424d3dfe31e5fe1f885db7e10b6d5733ece
tamirz12345 <tamirz12345@gmail.com>
2015-05-14 12:54:29 +0300

    UnitConvertor class update
    Now able to draw, this will be used to draw things to fit the view port

--- ClientSide/core/src/com/mygdx/Screens/UnitConvertor.java
+++ ClientSide/core/src/com/mygdx/Screens/UnitConvertor.java
@@ -1,8 +1,11 @@
 package com.mygdx.Screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
+
 
 //Class that converts cordinates of game to cordinates of math
 public class UnitConvertor {
@@ -21,7 +24,11 @@ public class UnitConvertor {
 		Vector2 temp =   toNormal(param.x, param.y);
 		return new Vector3(temp.x,temp.y,0);
 	}
-	
+	public static void draw(SpriteBatch batch ,Texture t,float x , float y , float w , float h )
+	{
+		Vector2 pos = UnitConvertor.toGame(x, y + h);
+		batch.draw(t ,pos.x,  pos.y , w , h);
+	}
 	public static Vector2 toGame(float x , float y)
 	{
 		Vector2 v = new Vector2();
